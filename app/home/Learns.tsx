2c830fd47d1c1baefd4ed2362c0659f186078044
Amer-Alghfili <ameralghfili@gmail.com>
2025-11-24 00:10:53 +0300

    style list of learns

--- app/home/Learns.tsx
+++ app/home/Learns.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
+import { EditIcon } from "@/components/Icons";
 import { prisma } from "@/prisma";
-import { Box, Stack } from "@chakra-ui/react";
+import { Button, Card, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 
 export async function Learns() {
   const data = await auth();
@@ -15,12 +16,25 @@ export async function Learns() {
   });
 
   //TODO: empty state
-  return learns.map((learn) => {
+  return learns.map(({ id, title, description }) => {
     return (
-      <Stack key={learn.id}>
-        <Box>{learn.title}</Box>
-        <Box>{learn.description}</Box>
-      </Stack>
+      <Card.Root key={id} w="full">
+        <Card.Body>
+          <Stack gap={0}>
+            <Flex alignItems="center" gap="1em" justifyContent="space-between">
+              <Heading as="h4">{title}</Heading>
+              <Button
+                variant="plain"
+                p={0}
+                _hover={{ bg: "rgb(152, 109, 0, 0.2)" }}
+              >
+                <EditIcon w="24px" h="24px" />
+              </Button>
+            </Flex>
+            <Text maxW="31.25rem">{description}</Text>
+          </Stack>
+        </Card.Body>
+      </Card.Root>
     );
   });
 }
