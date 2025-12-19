7416d85a7f2d715e5660376200cd6c9caa9f0c91
Amer-Alghfili <ameralghfili@gmail.com>
2025-12-19 17:17:10 +0300

    fix build issue

--- app/learns/[id]/knowledge-test/flash-cards/index.tsx
+++ app/learns/[id]/knowledge-test/flash-cards/index.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/prisma";
 import { Container } from "./Container";
+import { AnswerType } from "./actions";
 
 export async function FlashCards({ learnId }: { learnId: number }) {
   const res = await prisma.flashCard.findMany({
@@ -11,5 +12,13 @@ export async function FlashCards({ learnId }: { learnId: number }) {
     },
   });
 
-  return <Container flashCards={res} learnId={learnId} />;
+  return (
+    <Container
+      flashCards={res.map((r) => ({
+        ...r,
+        answerType: r.answerType as AnswerType,
+      }))}
+      learnId={learnId}
+    />
+  );
 }
