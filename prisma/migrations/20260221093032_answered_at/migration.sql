9a0f15378628ff2505fd3bc06d0825f3290fa1f5
Amer-Alghfili <ameralghfili@gmail.com>
2026-02-21 12:30:52 +0300

    add answeredAt column

---
+++ prisma/migrations/20260221093032_answered_at/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "learns_flash_cards" ADD COLUMN     "answeredAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "learns_practice_tasks" ADD COLUMN     "answeredAt" TIMESTAMP(3);
